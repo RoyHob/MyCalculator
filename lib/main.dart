d8a83a4b4cc067abec423c237917d7e9904e55ae
FlutLab.io <>
2021-12-29 22:14:05 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'my_calculator.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Z Calculator',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyCalculator());
+  }
+}
---
+++ lib/my_calculator.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+
+class MyCalculator extends StatefulWidget {
+  @override
+  MyCalculatorState createState() => MyCalculatorState();
+}
+
+class MyCalculatorState extends State<MyCalculator> {
+  Widget buildButton(String buttonText, double buttonHeight, Color buttonColor) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.1 * buttonHeight,
+      color: buttonColor,
+      child: FlatButton(
+        onPressed: null,
+        child: Text(
+          buttonText,
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text("Z Calculator"),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                alignment: Alignment.centerRight,
+                padding: EdgeInsets.fromLTRB(
+                  10,
+                  10,
+                  10,
+                  10,
+                ),
+                color: Colors.redAccent.withOpacity(0.7),
+                child: Text("0", style: TextStyle(fontSize: 30)),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                alignment: Alignment.centerRight,
+                padding: EdgeInsets.fromLTRB(
+                  20,
+                  20,
+                  10,
+                  20,
+                ),
+                color: Colors.greenAccent.withOpacity(0.7),
+                child: Text("0", style: TextStyle(fontSize: 40)),
+              ),
+            ),
+            Expanded(child: Divider()),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.75,
+                  child: Table(
+                    children: [
+                      TableRow(
+                        children: [
+                          buildButton("C", 1, Colors.red),
+                          buildButton("Back", 1, Colors.blue),
+                          buildButton("/", 1, Colors.blue)
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          buildButton("7", 1, Colors.grey),
+                          buildButton("8", 1, Colors.grey),
+                          buildButton("9", 1, Colors.grey)
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          buildButton("4", 1, Colors.grey),
+                          buildButton("5", 1, Colors.grey),
+                          buildButton("6", 1, Colors.grey)
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          buildButton("1", 1, Colors.grey),
+                          buildButton("2", 1, Colors.grey),
+                          buildButton("3", 1, Colors.grey)
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          buildButton(".", 1, Colors.grey),
+                          buildButton("0", 1, Colors.grey),
+                          buildButton("00", 1, Colors.grey)
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.25,
+                  child: Table(
+                    children: [
+                      TableRow(
+                        children: [
+                          buildButton("*", 1, Colors.blue)
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          buildButton("-", 1, Colors.blue)
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          buildButton("+", 1, Colors.blue)
+                        ],
+                      ),
+                      TableRow(
+                        children: [
+                          buildButton("=", 2, Colors.red)
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
